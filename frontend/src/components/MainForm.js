c17da1eafa2b2e734aebdaf0bd21154f7844ca95
Shawn Zhuang <cubershz@gmail.com>
2023-02-18 03:25:24 -0800

    Move MainForm into its own function

---
+++ frontend/src/components/MainForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './MainForm.css'
+import Button from './Button'
+
+export default function MainForm() {
+  const [searchText, setSearchText] = useState('')
+  const [quantity, setQuantity] = useState('')
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    // handle search logic
+  };
+
+  const handleSimpleSearch = () => {
+    // hand search options
+    console.log("launching simple search");
+  };
+
+  const handleFilteredSearch = () => {
+    // hand search options
+    console.log("launching filtered search");
+  };
+  
+  return (
+    <form id="main-page-form" onSubmit={handleSearch}>
+      <div id="form-elements-container">
+        <div id="search-input-container">
+            <input id="search-input" placeholder="search" type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+        </div>
+        <br />
+        <div id="quantity-input-container">
+          <span id="quantity-span"> 
+            Quantity:
+          </span>
+          <input type="number" max="100" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
+        </div>
+        <br />
+        <div id="buttons">
+          <Button handleClick={handleSimpleSearch}>
+            Simple Search
+          </Button>
+          <Button handleClick={handleFilteredSearch}>
+            Filtered Search
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+};
