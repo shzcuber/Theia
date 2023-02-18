177dbeead0935d3ec2497e79a405a91fa1284cfe
Shawn Zhuang <cubershz@gmail.com>
2023-02-18 01:00:18 -0800

    Organize App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,28 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
+  const handleSearchOptions = () => {
+    // hand search options
+    console.log("launching search options");
+  };
+
+  return (
+    <div className="App">
+      <Button handleClick={handleSearchOptions}>
+        Search Options
+      </Button>
+      <MainForm />
+    </div>
+  );
+}
+
+function MainForm() {
   const [searchText, setSearchText] = useState('')
   const [quantity, setQuantity] = useState('')
-  const [filterSearch, setFilterSearch] = useState(false)
 
   const handleSearch = (event) => {
     event.preventDefault();
     // handle search logic
-  };
-
-  const handleSearchOptions = () => {
-    // hand search options
-    console.log("launching search options");
   };
 
   const handleSimpleSearch = () => {
@@ -27,34 +37,29 @@ function App() {
     // hand search options
     console.log("launching filtered search");
   };
-
+  
   return (
-    <div className="App">
-      <form onSubmit={handleSearch}>
-        <div style={{ textAlign: 'center' }}>
-          <label>
-            Search:
-            <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Quantity:
-            <input type="number" max="100" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
-          </label>
-          <br />
-          <Button handleClick={handleSimpleSearch}>
-            Simple Search
-          </Button>
-          <Button handleClick={handleFilteredSearch}>
-            Filtered Search
-          </Button>
-        </div>
-      </form>
-      <Button handleClick={handleSearchOptions}>
-        Search Options
-      </Button>
-    </div>
+    <form onSubmit={handleSearch}>
+      <div style={{ textAlign: 'center' }}>
+        <label>
+          Search:
+          <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Quantity:
+          <input type="number" max="100" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
+        </label>
+        <br />
+        <Button handleClick={handleSimpleSearch}>
+          Simple Search
+        </Button>
+        <Button handleClick={handleFilteredSearch}>
+          Filtered Search
+        </Button>
+      </div>
+    </form>
   );
-}
+};
 
 export default App;
